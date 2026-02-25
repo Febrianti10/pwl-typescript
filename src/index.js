e400cb180992af6832ffdac75b3b2e7e6b18ce61
Febrianti10 <155545517+Febrianti10@users.noreply.github.com>
2026-02-25 04:44:39 +0000

    commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function hello(nama) {
+    return 'Selamat datang ' + nama + ' di kelas pwl, betah-betah ya';
+}
+console.log(hello('John'));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+function hello(nama: string, umur: number): string {
+    return 'Selamat datang '+nama+' di kelas pwl, betah-betah ya, umur Ali '+umur+'';
+}
+const mahasiswa = "Ali";
+const umur:number = 19;
+console.log(hello(mahasiswa, umur));
